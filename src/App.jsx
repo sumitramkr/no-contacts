dd8ed405eff4fd770f6895c04e91cba24a2cd1d9
Sumitram Kumar <sumitramkr.dev@gmail.com>
2024-05-20 17:46:33 +0530

    chore: Update bootstrap dependency and import statements

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Mobileform from "./Mobileform";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
